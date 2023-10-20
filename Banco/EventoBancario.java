e604c8b6e1779af2e4cdf253f79e9ed60d609a6b
NefiValderrama <142445288+NefiValderrama@users.noreply.github.com>
2023-10-19 19:58:03 -0500

    Update EventoBancario.java
    Nefi

--- Banco/EventoBancario.java
+++ Banco/EventoBancario.java
@@ -1,4 +1,36 @@
 package Banco;
+import java.util.Date;
+
 public class EventoBancario {
-    
+    private Date fecha;
+    private String tituloEvento;
+    private String descripcionEvento;
+
+    public EventoBancario(Date fecha, String tituloEvento, String descripcionEvento){
+        this.fecha = fecha;
+        this.tituloEvento = tituloEvento;
+        this.descripcionEvento = descripcionEvento;   
+    }
+    public Date getFecha(){
+        return fecha;
+    }
+    public void setFecha(Date fecha){
+        this.fecha = fecha;
+    }
+    public String getTituloEvento(){
+        return tituloEvento;
+    }
+    public void setTituloEvento(String tituloEvento){
+        this.tituloEvento = tituloEvento;
+    }
+    public String getDescripcionEvento(){
+        return descripcionEvento;
+    }
+    public void setDescripcionEvento(String descripcionEvento){
+        this.descripcionEvento = descripcionEvento;
+    }
+    @Override
+    public String toString() {
+        return "Fecha: " + fecha + "\nTitulo del evento: " + tituloEvento + "\nDescripción del evento: " + descripcionEvento;
+    }
 }
